ed3bf63a1d8b884cf1f4441899ab66adbb29d36d
_why <why@whytheluckystiff.net>
2007-07-31 00:06:13 +0000

    * samples: the timer sample and a test of all dialog boxes.

---
+++ samples/dialogs.rb
@@ -0,0 +1,23 @@
+Shoes.app :width => 300, :height => 150, :margin => 10 do
+  def answer(v)
+    @answer.markup = "<span color='black'>#{v.inspect}</span>"
+  end
+
+  button "Ask" do
+    answer ask("What is your name?")
+  end
+  button "Confirm" do
+    answer confirm("Would you like to proceed?")
+  end
+  button "Open File..." do
+    answer ask_open_file
+  end
+  button "Save File..." do
+    answer ask_save_file
+  end
+  button "Color" do
+    answer ask_color("Pick a Color")
+  end
+
+  @answer = markup("<span color='black'>Answers appear here</span>")
+end
---
+++ samples/timer.rb
@@ -0,0 +1,14 @@
+label, time = nil, Time.now
+Shoes.app :height => 150, :width => 250 do
+  background "rgb(240, 250, 208)"
+  stack :margin => 10 do
+    button "Start" do
+      time = Time.now
+      label.replace "Stop watch started at #{time}"
+    end
+    button "Stop" do
+      label.replace "Stopped, #{Time.now - time} seconds elapsed."
+    end
+    label = text "Press start to begin timing."
+  end
+end
