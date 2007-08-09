711beddb64d911f1d976d23bc496122a2ba7257d
_why <why@whytheluckystiff.net>
2007-08-09 18:57:06 +0000

    * samples/form.rb: updated form sample.

---
+++ samples/form.rb
@@ -0,0 +1,29 @@
+name, phone, address = nil
+Shoes.app do
+  background "static/menu-gray.png"
+  background "static/menu-top.png", :height => 50
+  background "static/menu-left.png", :top => 50, :width => 55
+  background "static/menu-right.png", :right => 0, :top => 50, :width => 55
+  image "static/menu-corner1.png", :top => 0, :left => 0
+  image "static/menu-corner2.png", :right => 0, :top => 0
+
+  stack :marginleft => 20 do
+    stack :margin => 10 do
+      text "Name"
+      name = list_box :items => ["Yes, please!", "NO.  No thankyou."]
+    end
+    stack :margin => 10 do
+      text "Address"
+      address = edit_line
+    end
+    stack :margin => 10 do
+      text "Phone"
+      phone = edit_line
+    end
+    stack :margin => 10 do
+      button "Save" do
+        Shoes.p [name.text, address.text, phone.text]
+      end
+    end
+  end
+end
