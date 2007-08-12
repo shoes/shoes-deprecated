05594be0a92e55823675302832c8a5b81438e09c
_why <why@whytheluckystiff.net>
2007-08-12 00:09:55 +0000

    * samples/anim.rb: just messing with animating the font size.

--- samples/anim.rb
+++ samples/anim.rb
@@ -1,9 +1,13 @@
 Shoes.app do
-  l = text "0"
-  animate(24) do |i|
-    l.replace i.inspect
-  end
-  motion do |x, y|
-    Shoes.p [x, y]
+  stack :top => 0.5, :left => 0.5 do
+    text "Counting up:"
+    l = text "0"
+    animate(24) do |i|
+      f = ['Arial 14px', 'Serif 34px', 'Monospace 18px', 'Arial 48px'][rand(3)]
+      l.replace "<span font_desc='#{f}'>#{i}</span>"
+    end
+    motion do |x, y|
+      Shoes.p [x, y]
+    end
   end
 end
