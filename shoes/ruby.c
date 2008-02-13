53b8c5bea3275177d022ac607bf72508072c392f
_why <why@whytheluckystiff.net>
2008-02-13 21:06:15 +0000

    * shoes/ruby.c: casting error on osx.

--- shoes/ruby.c
+++ shoes/ruby.c
@@ -3240,7 +3240,7 @@ shoes_progress_set_fraction(VALUE self, VALUE _perc)
     gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(self_t->ref), perc);
 #endif
 #ifdef SHOES_QUARTZ
-    SetControl32BitValue(self_t->ref, SInt32(perc) * 100.);
+    SetControl32BitValue(self_t->ref, (SInt32)(perc * 100.));
 #endif
 #ifdef SHOES_WIN32
     SendMessage(self_t->ref, PBM_SETPOS, (int)(perc * 100), 0L);
