ed792842cb172a99b9ff79a9e50864cc3eac1f23
_why <why@whytheluckystiff.net>
2008-05-01 01:06:44 -0600

    * samples/simple-mask.rb: animate and add a background.

--- samples/simple-mask.rb
+++ samples/simple-mask.rb
@@ -1,11 +1,21 @@
 Shoes.app do
-  20.times do |i|
-    strokewidth 4
-    stroke rgb((0.0..0.5).rand, (0.0..1.0).rand, (0.0..0.3).rand)
-    line 0, i * 4, 400, i * 4
-  end
+  background black
 
-  mask do
-    title "Shoes"
+  stack :top => 0.4, :left => 0.4 do
+    @stripes = stack
+
+    mask do
+      title "Shoes", :weight => "bold"
+    end
+  end
+  
+  animate 10 do
+    @stripes.clear do
+      20.times do |i|
+        strokewidth 4
+        stroke rgb((0.0..0.5).rand, (0.0..1.0).rand, (0.0..0.3).rand)
+        line 0, i * 4, 400, i * 4
+      end
+    end
   end
 end
