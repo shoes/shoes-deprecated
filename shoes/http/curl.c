cdf7cd07f50bbc58d9be004a67d6ca3ebcc786c1
_why <why@whytheluckystiff.net>
2008-11-13 16:29:47 -0800

    * shoes/http/curl.c: fix :body option on `download` for curl.

--- shoes/http/curl.c
+++ shoes/http/curl.c
@@ -18,7 +18,7 @@ typedef struct {
   unsigned long memlen;
   FILE *fp;
   char *body;
-  size_t size, total, readpos;
+  size_t size, total, readpos, bodylen;
   unsigned long status;
   shoes_download_handler handler;
   SHOES_TIME last;
@@ -61,6 +61,8 @@ shoes_curl_read_funk(void *ptr, size_t size, size_t nmemb, void *user)
 {
   shoes_curl_data *data = (shoes_curl_data *)user;
   size_t realsize = size * nmemb;
+  if (data->readpos == data->bodylen) return 0;
+  if (data->readpos + realsize > data->bodylen) realsize = data->bodylen - data->readpos;
   SHOE_MEMCPY(ptr, &(data->body[data->readpos]), char, realsize);
   data->readpos += realsize;
   return realsize;
@@ -111,7 +113,7 @@ shoes_download(shoes_download_request *req)
   char url[SHOES_BUFSIZE], uagent[SHOES_BUFSIZE], slash[2] = "/";
   CURL *curl = curl_easy_init();
   CURLcode res;
-  shoes_curl_data cdata;
+  shoes_curl_data *cdata = SHOE_ALLOC(shoes_curl_data);
   if (curl == NULL) return;
 
   if (req->path[0] == '/') slash[0] = '\0';
@@ -119,34 +121,34 @@ shoes_download(shoes_download_request *req)
   sprintf(uagent, "Shoes/0.r%d (%s) %s/%d", SHOES_REVISION, SHOES_PLATFORM,
     SHOES_RELEASE_NAME, SHOES_BUILD_DATE);
 
-  cdata.mem = req->mem;
-  cdata.memlen = req->memlen;
-  cdata.fp = NULL;
-  cdata.size = cdata.readpos = req->size = 0;
-  cdata.total = 0;
-  cdata.handler = req->handler;
-  cdata.data = req->data;
-  cdata.last.tv_sec = 0;
-  cdata.last.tv_nsec = 0;
-  cdata.status = 0;
-  cdata.curl = curl;
-  cdata.body = NULL;
+  cdata->mem = req->mem;
+  cdata->memlen = req->memlen;
+  cdata->fp = NULL;
+  cdata->size = cdata->readpos = req->size = 0;
+  cdata->total = 0;
+  cdata->handler = req->handler;
+  cdata->data = req->data;
+  cdata->last.tv_sec = 0;
+  cdata->last.tv_nsec = 0;
+  cdata->status = 0;
+  cdata->curl = curl;
+  cdata->body = NULL;
 
   if (req->mem == NULL)
   {
-    cdata.fp = fopen(req->filepath, "wb");
-    if (cdata.fp == NULL) return;
+    cdata->fp = fopen(req->filepath, "wb");
+    if (cdata->fp == NULL) goto done;
   } 
 
   curl_easy_setopt(curl, CURLOPT_NOSIGNAL, TRUE);
   curl_easy_setopt(curl, CURLOPT_URL, url);
   curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, shoes_curl_header_funk);
-  curl_easy_setopt(curl, CURLOPT_WRITEHEADER, &cdata);
+  curl_easy_setopt(curl, CURLOPT_WRITEHEADER, cdata);
   curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, shoes_curl_write_funk);
-  curl_easy_setopt(curl, CURLOPT_WRITEDATA, &cdata);
+  curl_easy_setopt(curl, CURLOPT_WRITEDATA, cdata);
   curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0L);
   curl_easy_setopt(curl, CURLOPT_PROGRESSFUNCTION, shoes_curl_progress_funk);
-  curl_easy_setopt(curl, CURLOPT_PROGRESSDATA, &cdata);
+  curl_easy_setopt(curl, CURLOPT_PROGRESSDATA, cdata);
   curl_easy_setopt(curl, CURLOPT_USERAGENT, uagent);
   if (req->flags & SHOES_DL_REDIRECTS)
     curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
@@ -156,14 +158,15 @@ shoes_download(shoes_download_request *req)
     curl_easy_setopt(curl, CURLOPT_HTTPHEADER, req->headers);
   if (req->body)
   {
-    cdata.body = req->body;
+    cdata->body = req->body;
+    curl_easy_setopt(curl, CURLOPT_UPLOAD, 1);
     curl_easy_setopt(curl, CURLOPT_READFUNCTION, shoes_curl_read_funk);
-    curl_easy_setopt(curl, CURLOPT_READDATA, &cdata);
+    curl_easy_setopt(curl, CURLOPT_READDATA, cdata);
   }
 
   res = curl_easy_perform(curl);
-  req->size = cdata.size;
-  req->mem = cdata.mem;
+  req->size = cdata->size;
+  req->mem = cdata->mem;
 
   if (res != CURLE_OK)
   {
@@ -174,17 +177,19 @@ shoes_download(shoes_download_request *req)
     goto done;
   }
 
-  if (cdata.fp != NULL)
+  if (cdata->fp != NULL)
   { 
-    fclose(cdata.fp);
-    cdata.fp = NULL;
+    fclose(cdata->fp);
+    cdata->fp = NULL;
   }
 
-  HTTP_EVENT(cdata.handler, SHOES_HTTP_COMPLETED, cdata.last, 100, req->size, req->size, cdata.data, req->mem, goto done);
+  HTTP_EVENT(cdata->handler, SHOES_HTTP_COMPLETED, cdata->last, 100, req->size, req->size, cdata->data, req->mem, goto done);
 
 done:
-  if (cdata.fp != NULL)
-    fclose(cdata.fp);
+  if (cdata->fp != NULL)
+    fclose(cdata->fp);
+  if (cdata != NULL)
+    SHOE_FREE(cdata);
 
   curl_easy_cleanup(curl);
 }
