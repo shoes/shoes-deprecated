df45d67505de99ed08cba3074b95f254e6742d59
ender672 <tle@holymonkey.com>
2009-01-20 14:23:20 -0800

    Get shoes dll to compile, and shoes.exe to build. YAY, IT RUNS!!
    This commit gets shoes.exe to build and it runs and I tested with a few apps.

--- shoes/config.h
+++ shoes/config.h
@@ -166,7 +166,7 @@ typedef struct {
 #define SHOES_CONTROL1   3045
 #define SHOES_WM_MESSAGE (WM_APP + 3045)
 #define SHOES_INIT_ARGS  HINSTANCE inst, int style
-#define SHOES_EXTERN
+#define SHOES_EXTERN __declspec(dllexport)
 
 typedef struct {
   PAINTSTRUCT ps;
