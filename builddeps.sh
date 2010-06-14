67b1eaa66616d371d3a9c23eb927f896dd8bf451
Steve Klabnik <steve@steveklabnik.com>
2010-06-14 10:14:52 +0800

    this is the first version of a script that automates building deps on snow leopard

---
+++ builddeps.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+rm -rf /tmp/deps
+cd /usr/local/src
+
+#make clean all deps
+cd pkg-config-0.20
+make clean
+cd ../libpng-1.4.1
+make clean
+cd ../jpeg-8a
+make clean
+cd ../giflib-4.1.6
+make clean
+cd ../gettext-0.17
+make clean
+cd ../libiconv-1.13
+make clean
+cd ../glib-2.24.0
+make clean
+cd ../pixman-0.18.0
+make clean
+cd ../cairo-1.8.10
+make clean
+cd ../pango-1.28.0
+make clean
+cd ../portaudio
+make clean
+cd ../ruby-1.9.1-p378
+make clean
+
+#build pkg-config
+cd ../pkg-config-0.20
+./configure --prefix=/tmp/dep
+make && make install
+export MACOSX_DEPLOYMENT_TARGET=10.6
+export PATH=/tmp/dep/bin:$PATH
+export CFLAGS=-I/tmp/dep/include
+export LDFLAGS=-L/tmp/dep/lib
+export PKG_CONFIG_PATH=/tmp/dep/lib/pkgconfig
+
+#build libpng
+cd ../libpng-1.4.1
+./configure --prefix=/tmp/dep
+make && make install
+
+#build libjpeg
+cd ../jpeg-8a/
+./configure --prefix=/tmp/dep --enable-shared
+make && make install
+
+#build giflib
+cd ../giflib-4.1.6
+./configure --prefix=/tmp/dep
+make && make install
+
+#build gettext
+cd ../gettext-0.17
+./configure --prefix=/tmp/dep
+make && make install
+
+#build libiconv
+cd ../libiconv-1.13
+./configure --prefix=/tmp/dep
+make && make install
+
+#build glib 
+cd ../glib-2.24.0
+./configure --prefix=/tmp/dep
+make && make install
+
+#build pixman 
+cd ../pixman-0.18.0
+./configure --prefix=/tmp/dep
+make && make install
+
+#build cairo 
+cd ../cairo-1.8.10
+./configure --prefix=/tmp/dep --enable-quartz=yes --enable-quartz-font=yes --enable-xlib=no
+make && make install
+
+#build pango 
+cd ../pango-1.28.0
+./configure --prefix=/tmp/dep --with-x=no
+make && make install
+
+#build Port Audio 
+cd ../portaudio/
+./configure --disable-mac-universal --prefix=/tmp/dep
+ke && make install
+
+#build ruby
+cd ../ruby-1.9.1-p378
+./configure --prefix=/tmp/dep --enable-shared
+make && make install
+
+#install RubyGems.
+/tmp/dep/bin/ruby setup.rb
+
+#install rake.
+/tmp/dep/bin/gem install rake
