b0e8e3303716182815a8a426fe96c92b2d6c6201
ashbb <ashbbb@gmail.com>
2010-08-11 13:54:24 +0900

    Edited a little bit for Windows MinGW:
    - shoes/make/rakefile_common : deleted (duplicated file)
    - shoes/make/mingw/tasks.rb  : to cut off new lines
    - shoes/make/mingw/env.rb    : to use copy_files method

--- make/mingw/tasks.rb
+++ make/mingw/tasks.rb
@@ -22,7 +22,7 @@ class MakeMinGW
 
     def copy_deps_to_dist
       dlls = [RUBY_SO]
-      dlls += IO.readlines("make/mingw/dlls")
+      dlls += IO.readlines("make/mingw/dlls").map{|dll| dll.chomp}
       dlls += %w{libvlc} if ENV['VIDEO']
       dlls.each{|dll| cp "#{EXT_RUBY}/bin/#{dll}.dll", "dist/"}
       cp "dist/zlib1.dll", "dist/zlib.dll"
