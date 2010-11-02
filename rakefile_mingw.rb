c86ab9b16178938a15fa472fcde1ee37694cce22
Konstantin Shabanov <kes.eclipse@gmail.com>
2010-11-03 00:42:57 +0600

    fix building on 1.9.2

--- rakefile_mingw.rb
+++ rakefile_mingw.rb
@@ -1,6 +1,6 @@
-require 'make/rakefile_common'
-require 'make/mingw/env'
-require 'make/mingw/tasks'
+require File.expand_path('make/rakefile_common')
+require File.expand_path('make/mingw/env')
+require File.expand_path('make/mingw/tasks')
 
 rule ".o" => ".rc" do |t|
   MakeMinGW.make_resource t
