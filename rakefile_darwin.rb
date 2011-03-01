481f7d1a83c0aa7c9f878013bd0944b945360518
Steve Klabnik <steve@steveklabnik.com>
2011-02-28 22:55:09 -0500

    Cleaning up the Rakefile situation.

--- rakefile_darwin.rb
+++
@@ -1,7 +0,0 @@
-require File.expand_path('make/rakefile_common')
-require File.expand_path('make/darwin/env')
-require File.expand_path('make/darwin/tasks')
-
-task :stub do
-  MakeDarwin.make_stub
-end
--- rakefile_mingw.rb
+++
@@ -1,7 +0,0 @@
-require File.expand_path('make/rakefile_common')
-require File.expand_path('make/mingw/env')
-require File.expand_path('make/mingw/tasks')
-
-rule ".o" => ".rc" do |t|
-  MakeMinGW.make_resource t
-end
