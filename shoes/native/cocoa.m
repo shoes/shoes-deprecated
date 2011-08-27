590ef990517852995230d47b251bb025c309881c
Eric Watson <wasnotrice@gmail.com>
2011-08-26 23:06:27 -0500

    Remove 4 compile warnings

--- shoes/native/cocoa.m
+++ shoes/native/cocoa.m
@@ -116,11 +116,11 @@
   Data_Get_Struct(app, shoes_app, a);
   Data_Get_Struct(a->canvas, shoes_canvas, canvas);
   if (type == s_motion)
-    shoes_app_motion(a, p.x, (canvas->height - p.y) + canvas->slot->scrolly);
+    shoes_app_motion(a, (int)round(p.x), (canvas->height - (int)round(p.y)) + canvas->slot->scrolly);
   else if (type == s_click)
-    shoes_app_click(a, b, p.x, (canvas->height - p.y) + canvas->slot->scrolly);
+    shoes_app_click(a, b, (int)round(p.x), (canvas->height - (int)round(p.y)) + canvas->slot->scrolly);
   else if (type == s_release)
-    shoes_app_release(a, b, p.x, (canvas->height - p.y) + canvas->slot->scrolly);
+    shoes_app_release(a, b, (int)round(p.x), (canvas->height - (int)round(p.y)) + canvas->slot->scrolly);
 }
 - (void)mouseDown: (NSEvent *)e
 {
