6a29fcf13d83a8faba031f88dafef777edc8ea5f
Cecil Coupe <ccoupe@cableone.net>
2012-02-07 20:55:59 -0700

    Update the cross compile rakefiles with tweaks. There will be more clean ahead.

--- make/xmingw/env.rb
+++ make/xmingw/env.rb
@@ -1,11 +1,17 @@
 require File.expand_path('make/make')
 extend Make
+# set the absolute path to the deps library
+XDEPABS = '/home/ccoupe/Projects/xmingw'
 
-EXT_RUBY = "../mingw"
+# Set where the cross built Ruby lives
+# The direcory with gems and libs 
+EXT_RUBY = "../xmingw/ruby-1.9.2-p290"
 
 # use the platform Ruby claims
 #require 'rbconfig'
 #CC = ENV['CC'] ? ENV['CC'] : "gcc"
+# Better yet, get the rbconfig of the cross compiled ruby
+require "#{EXT_RUBY}/rbconfig"
 
 # set a prefix path to where your cross compiler and tools live
 XTOOLS = '/usr/bin/i586-mingw32msvc-'
@@ -13,8 +19,7 @@ XTOOLS = '/usr/bin/i586-mingw32msvc-'
 CC = "#{XTOOLS}cc"
 
 # set where your dependent code like cairo is kept. No slash at the end.
-XDEP = "/home/ccoupe/Projects/xmingw"
-
+XDEP = "../xmingw"
 # set where the .h are 
 XINC = "#{XDEP}/include"
 # set where the dll that Shoes doesn't compile live at.
@@ -25,8 +30,9 @@ XRUBYINC = "#{XINC}/ruby-1.9.1"
 # Where the Wine .h files are
 XWINH = "#{XDEP}/wineh"
 
-#file_list = ["shoes/*.c"] + %w{shoes/native/windows.c shoes/http/winhttp.c shoes/http/windownload.c}
-file_list = ["shoes/*.c"] + %w{shoes/native/windows.c shoes/http/curl.c}
+file_list = ["shoes/*.c"] + %w{shoes/native/windows.c shoes/http/winhttp.c shoes/http/windownload.c}
+# Building with curl doesn't work yet.
+#file_list = ["shoes/*.c"] + %w{shoes/native/windows.c shoes/http/curl.c}
   
 SRC = FileList[*file_list]
 OBJ = SRC.map do |x|
@@ -37,11 +43,11 @@ ADD_DLL = ["shoes/appwin32.o"]
 
 # Linux build environment for MingW
 #CAIRO_CFLAGS = "-I/mingw/include/glib-2.0 -I/mingw/lib/glib-2.0/include -I/mingw/include/cairo"
-CAIRO_CFLAGS = "-I/#{XINC}/glib-2.0 -I/#{XLIB}/glib-2.0/include -I#{XINC}/cairo"
+CAIRO_CFLAGS = "-I#{XINC}/glib-2.0 -I#{XLIB}/glib-2.0/include -I#{XINC}/cairo"
 CAIRO_LIB = '-lcairo'
 PANGO_CFLAGS = "-I#{XINC}/pango-1.0"
 PANGO_LIB = '-lpangocairo-1.0 -lpango-1.0 -lpangoft2-1.0 -lpangowin32-1.0'
-LINUX_CFLAGS = "-I#{XRUBYINC} #{CAIRO_CFLAGS} #{PANGO_CFLAGS}"
+LINUX_CFLAGS = "-I#{XINC} -I#{XRUBYINC} #{CAIRO_CFLAGS} #{PANGO_CFLAGS}"
 LINUX_CFLAGS << " -I#{XRUBYINC}/i386-mingw32 -I#{XWINH}"
 #LINUX_CFLAGS = %[-Wall -I#{ENV['SHOES_DEPS_PATH'] || "/usr"}/include #{CAIRO_CFLAGS} #{PANGO_CFLAGS} -I#{Config::CONFIG['archdir']}]
 #if Config::CONFIG['rubyhdrdir']
@@ -65,11 +71,12 @@ DLEXT = 'dll'
 #LINUX_CFLAGS << ' -I/mingw/include/ruby-1.9.1/ruby'
 LINUX_CFLAGS << " -DXMD_H -DHAVE_BOOLEAN -DSHOES_WIN32 -D_WIN32_IE=0x0500 -D_WIN32_WINNT=0x0500 -DWINVER=0x0500 -DCOBJMACROS"
 LINUX_LDFLAGS = " -DBUILD_DLL -L#{XLIB} -lungif -ljpeg -lglib-2.0 -lgobject-2.0 -lgio-2.0 -lgmodule-2.0 -lgthread-2.0 -fPIC -shared"
-LINUX_LDFLAGS << ' --enable-auto-import -lshell32 -lkernel32 -luser32 -lgdi32 -lcomdlg32 -lcomctl32 -lole32 -loleaut32 -ladvapi32 -loleacc' # -lwinhttp' # problem expected winhttp
+LINUX_LDFLAGS << ' --enable-auto-import -lshell32 -lkernel32 -luser32 -lgdi32 -lcomdlg32 -lcomctl32 -lole32 -loleaut32 -ladvapi32 -loleacc'
 
 cp APP['icons']['win32'], "shoes/appwin32.ico"
   
 LINUX_LIBS = LINUX_LIB_NAMES.map { |x| "-l#{x}" }.join(' ')
 
 #LINUX_LIBS << " -L#{Config::CONFIG['libdir']} #{CAIRO_LIB} #{PANGO_LIB}"
-LINUX_LIBS << " #{CAIRO_LIB} #{PANGO_LIB} -L#{XDEP}/ruby-1.9.2-p290 -lmsvcrt-ruby191" 
+LINUX_LIBS << " #{CAIRO_LIB} #{PANGO_LIB} -L#{XDEP}/bin -lmsvcrt-ruby191 #{XDEP}/lib/winhttp.a" 
+
--- rakefile_xmingw.rb
+++ rakefile_xmingw.rb
@@ -2,6 +2,6 @@ require File.expand_path('make/xmingw/env')
 require File.expand_path('make/xmingw/tasks')
 
 rule ".o" => ".rc" do |t|
-  MakeMinGW.make_resource t
+  MakeXMinGW.make_resource t
 end
 
