f1a07acdb2b9937c7cf0d8c082fd04e44b3e087d
ashbb <ashbbb@gmail.com>
2012-03-05 23:19:38 +0900

    The rb_unbound_get_class() in app.c didn't work with Ruby1.9.3p125. So, added a patch without using the function. Now class-book.rb works.

--- shoes/app.c
+++ shoes/app.c
@@ -369,7 +369,8 @@ shoes_app_visit(shoes_app *app, char *path)
   exec.block = rb_ary_entry(meth, 0);
   exec.args = rb_ary_entry(meth, 1);
   if (rb_obj_is_kind_of(exec.block, rb_cUnboundMethod)) {
-    VALUE klass = rb_unbound_get_class(exec.block);
+    //VALUE klass = rb_unbound_get_class(exec.block);
+    VALUE klass = rb_ary_entry(meth, 2);
     exec.canvas = app->nestslot = shoes_slot_new(klass, ssNestSlot, app->canvas);
     exec.block = rb_funcall(exec.block, s_bind, 1, exec.canvas);
     exec.ieval = 0;
