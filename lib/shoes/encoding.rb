75ae4a9b17d35aad650d71f3f8f1076a1f4b2265
Eric Watson <wasnotrice@gmail.com>
2012-03-23 15:20:55 -0500

    Make encoding.rb conditional for windows

--- lib/shoes/encoding.rb
+++ lib/shoes/encoding.rb
@@ -1,2 +1,5 @@
-IO.read(File.join DIR, 'encoding.data').
-each_line{|file| require File.join(DIR, 'ruby/lib/i386-mingw32/enc', file.chomp)}
+# Only needed on windows (encoding.data not installed for linux/darwin)
+if RbConfig::CONFIG['host_os'] =~ /mswin|mingw/
+  IO.read(File.join DIR, 'encoding.data').
+  each_line{|file| require File.join(DIR, 'ruby/lib/i386-mingw32/enc', file.chomp)}
+end
