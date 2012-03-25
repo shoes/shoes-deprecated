938e4700856bdb9c457425637334ba46396e14ac
ashbb <ashbbb@gmail.com>
2012-03-25 15:23:08 +0900

    Edited a little bit to build shoes.exe on Windows.

--- lib/shoes/encoding.rb
+++ lib/shoes/encoding.rb
@@ -1,5 +1,5 @@
 # Only needed on windows (encoding.data not installed for linux/darwin)
-if RbConfig::CONFIG['host_os'] =~ /mswin|mingw/
+if RUBY_PLATFORM =~ /mswin|mingw/
   IO.read(File.join DIR, 'encoding.data').
   each_line{|file| require File.join(DIR, 'ruby/lib/i386-mingw32/enc', file.chomp)}
 end
