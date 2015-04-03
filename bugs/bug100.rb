d9a4578361c3e0e560954011b62a08294a4e61f4
Cecil <ccoupe@cableone.net>
2015-04-02 23:36:48 -0600

    add bug/test snippets #97, #100

---
+++ bugs/bug100.rb
@@ -0,0 +1,9 @@
+Shoes.app width: 100, height: 100 do
+    stack width: 80, height: 80, margin: [10,10,0,0] do
+        background orange
+    end
+    stroke blue
+    strokewidth 1
+    nofill
+    rect 10,10,80,80
+end
