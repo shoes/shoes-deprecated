54a8c47d7f6a5b9f848e93b5410b1ed901d85c6d
Cecil <ccoupe@cableone.net>
2015-04-25 13:32:27 -0600

    use nokogiri if available fall back to hpricot.
    * should be the same as shoe4 code since it came from there.

--- samples/expert-funnies.rb
+++ samples/expert-funnies.rb
@@ -1,10 +1,16 @@
-require 'hpricot'
+# This example tries Nokogiri (used in Shoes 4), but falls back to
+# Hpricot for Shoes 3 compatibility
+begin
+  require 'nokogiri'
+rescue Exception => e
+  require 'hpricot'
+end
 
 class Comic
   attr_reader :rss, :title
 
   def initialize(body)
-    @rss = Hpricot.XML(body)
+    @rss = defined?(Nokogiri) ? Nokogiri::XML(body) : Hpricot(body)
     @title = @rss.at("//channel/title").inner_text
   end
 
@@ -17,34 +23,37 @@ class Comic
   end
 end
 
-Shoes.app :width => 800, :height => 600 do
+Shoes.app width: 800, height: 600 do
   background "#555"
 
   @title = "Web Funnies"
   @feeds = [
     "http://xkcd.com/rss.xml",
-    # feeds that crash Shoes
+    # No longer contains images
     #"http://feedproxy.google.com/DilbertDailyStrip?format=xml",
+    # debug
     #"http://www.smbc-comics.com/rss.php",
     #"http://www.daybydaycartoon.com/index.xml",
     #"http://www.questionablecontent.net/QCRSS.xml",
     #"http://indexed.blogspot.com/feeds/posts/default?alt=rss"
     ]
 
-  stack :margin => 10 do
-    title strong(@title), :align => "center", :stroke => "#DFA", :margin => 0
-    para "(loaded from RSS feeds)", :align => "center", :stroke => "#DFA",
-      :margin => 0
+  stack margin: 10 do
+    title strong(@title), align: "center", stroke: "#DFA", margin: 0
+    para "(loaded from RSS feeds)", align: "center", stroke: "#DFA",
+      margin: 0
 
     @feeds.each do |feed|
       download feed do |dl|
-        stack :width => "100%", :margin => 10, :border => 1 do
-          c = Comic.new dl.response.body
-          stack :margin_right => gutter do
-            background "#333", :curve => 4
-            caption c.title, :stroke => "#CD9", :margin => 4
+        stack width: "100%", margin: 10, border: 1 do
+          # Fall back to Shoes 3 syntax if necessary
+          body = dl.respond_to?(:read) ? dl.read : dl.response.body
+          c = Comic.new body
+          stack margin_right: gutter do
+            background "#333", curve: 4
+            caption c.title, stroke: "#CD9", margin: 4
           end
-          image c.latest_image.first, :margin => 8
+          image c.latest_image.first, margin: 8
         end
       end
     end
