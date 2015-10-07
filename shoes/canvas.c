8cc097309d0304154f61337ce38a2364c5a208eb
passenger94 <pierredavant@gmail.com>
2015-10-07 13:38:16 +0200

    fix #151

--- shoes/canvas.c
+++ shoes/canvas.c
@@ -1211,6 +1211,7 @@ shoes_canvas_remove(VALUE self)
     Data_Get_Struct(self_t->parent, shoes_canvas, pc);
     if (pc != self_t && DC(self_t->slot) != DC(pc->slot))
       shoes_slot_destroy(self_t, pc);
+    shoes_canvas_repaint_all(self);
   }
   shoes_canvas_send_finish(self);
   return self;
