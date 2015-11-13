cc9c3b309b83880a9e13399c1e53832fc3e0d90b
Cecil <ccoupe@cableone.net>
2015-11-13 00:10:14 -0700

    wait for graphics context before drawing an svg
    * still has weird behavior re events.

--- bugs/bug054.rb
+++ bugs/bug054.rb
@@ -1,13 +1,17 @@
 # simple test of svghandle class - full path name is important
 Shoes.app do
-  fpath = "/home/ccoupe/Projects/shoes3/icon/brownshoes.svg"
-  #svgh = svghandle({:filename => fpath})
-  #svgh.close
-  fl = File.open(fpath,"r");
-  bigstring = fl.read
-  fl.close
-  para "SVG is #{bigstring.length} long"
-  svg2 = svghandle({:from_string => bigstring})
-  svg2.draw
-  svg2.close
+  @slot = stack do
+    fpath = "/home/ccoupe/Projects/shoes3/icon/brownshoes.svg"
+    #svgh = svghandle({:filename => fpath})
+    #svgh.close
+    fl = File.open(fpath,"r");
+    bigstring = fl.read
+    fl.close
+    para "SVG is #{bigstring.length} long"
+    svg2 = svghandle({:from_string => bigstring})
+    button "draw" do
+      svg2.draw
+      #svg2.close
+    end
+  end
 end
