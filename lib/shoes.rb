7da38c9754d3c95faf57a779603b407c57729079
Cecil <ccoupe@cableone.net>
2015-12-22 02:14:42 -0700

    dumb error - splash.png should be relative to DIR. #149

--- lib/shoes.rb
+++ lib/shoes.rb
@@ -177,7 +177,7 @@ class Shoes
   def self.splash
     font "#{DIR}/fonts/Lacuna.ttf"
     Shoes.app width: 598, height: 520, resizable: false do
-      background "static/splash.png"
+      background "#{DIR}/static/splash.png"
       style(Para, align: 'center', weight: 'bold', font: 'Lacuna Regular', size: 13)
       style(Link, stroke: khaki, underline: nil)
       style(LinkHover, stroke: yellow, fill: nil)
