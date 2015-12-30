025562e5024dbdfb37a08ca01947345f6b458e7f
Cecil <ccoupe@cableone.net>
2015-12-30 00:39:51 -0700

    for #54 good-flip creates svghandle as needed instead of preloading
    * good startup speed on slow platforms
    * good drawing speed
    * needs a clever thread to started from init_with_path that loads missing
      handles while the user clicks. - That would make it expert-cardflip.rb

--- samples/good-flip.rb
+++ samples/good-flip.rb
@@ -3,9 +3,12 @@
 Shoes.app do
   @xmlstring = ''
   @suite_first = true
-  @handles = []
-  @pile = []
-  topcard = 0 # back of deck
+  @handles = [] # card handles by suite, rank. Numeric 0..52
+  @pile = []    # the deck of cards (shuffled?). Numeric 0..52
+  @topcard = 0  # numeric index of the handle that is currently shown in
+  @top_card = nil # the on screen svg widget
+  @names = []   # contains svg group ids (strings)
+  
   def shuffle_me(array)
     (array.size-1).downto(1) do |i|
      j = rand(i+1)
@@ -22,6 +25,16 @@ Shoes.app do
    end
   end
   
+  def get_handle (idx)
+    # create handle when needed
+    if @handles[idx] == nil
+      str = @names[idx]
+      puts "load: #{idx}:#{str}"
+      @handles[idx] = app.svghandle ( {content: @xmlstring, group: str} )
+    end
+    @handles[idx]
+  end
+  
   def init_with_path(path)
     File.open(path) {|f| @xmlstring = f.read}
     (1..52).each {|i| @pile[i] = i}
@@ -33,17 +46,25 @@ Shoes.app do
     else
       alert "Can't find order"
     end
-    @handles = [] # gc old ones
+    @handles = [] # let gc handle old ones
+    idx = 1
     @handles[0] = app.svghandle( {content: @xmlstring, group: '#back'})
+    @names[0] = "#back"
     ['club', 'diamond', 'heart', 'spade'].each do |suite|
       ['1','2','3','4','5','6','7','8','9', '10','jack', 'queen', 'king'].each do |card|
-        if @suite_first 
-          @handles << app.svghandle( {content: @xmlstring, group: "\##{suite}_#{card}"} )
-        else
-          @handles << app.svghandle( {content: @xmlstring, group: "\##{card}_#{suite}"} )
-        end
+        @names[idx] = @suite_first ? "\##{suite}_#{card}" : "\##{card}_#{suite}"
+        idx = idx + 1
       end
     end
+  end
+  
+  def tap 
+    kill_anim
+    @topcard = @topcard + 1
+    if @topcard == 52
+      @topcard = 0
+    end
+    @top_card.handle = get_handle @pile[@topcard]
   end
   
   if Shoes::RELEASE_TYPE =~ /TIGHT/
@@ -56,53 +77,51 @@ Shoes.app do
   end
   init_with_path(fpath)
 
-  #['joker_black', 'joker_red', 'back'].each do 
-  #end
   @animation = nil
-  @top_card = svg @handles[0], {width: 200, height: 200}
-  button "tap" do
-    kill_anim
-    topcard = topcard + 1
-    if topcard == 52
-      topcard = 0
+  flow do 
+    # display back of deck at startup
+    stack width: 200, height: 200 do
+      @top_card = svg @handles[0], {width: 200, height: 200, click: proc { tap }  }
     end
-    @top_card.handle = @handles[@pile[topcard]]
-  end
-  button "shuffle" do
-    kill_anim
-    temp_deck = []
-    (1..52).each {|i| temp_deck[i] = i}
-    @pile = shuffle_me(temp_deck)
-    run_for = 0
-    @animation = animate(10) do 
-      r = rand(52)
-      @top_card.handle = @handles[@pile[r+1]]
-      run_for = run_for + 1
-      if run_for >= 20
-        @animation.stop
-        top_card = 0
-        @top_card.handle = @handles[0]  # back of deck 
+    stack width: 100 do
+      button "shuffle" do
         kill_anim
+        temp_deck = []
+        (1..52).each {|i| temp_deck[i] = i}
+        @pile = shuffle_me(temp_deck)
+        run_for = 0
+        @animation = animate(10) do 
+          r = rand(52)
+          # @top_card.handle = @handles[@pile[r+1]]
+          @top_card.handle = get_handle @pile[r+1]
+          run_for = run_for + 1
+          if run_for >= 20
+            @animation.stop
+            top_card = 0
+            @top_card.handle = @handles[0]  # back of deck 
+            kill_anim
+          end
+        end
       end
-    end
-  end
-  button "reset" do
-    kill_anim
-    temp_deck = []
-    (1..52).each {|i| temp_deck[i] = i}
-    @pile = temp_deck
-    topcard = 0
-    @top_card.handle = @handles[0] # back of deck
-  end
-  button "quit" do
-    exit
-  end
-  button "Change Deck" do
-    fpath = ask_open_file
-    if fpath
-      init_with_path(fpath)
-      @top_card.handle = @handles[0]
-      topcard = 0
-    end
-  end
+      button "reset" do
+        kill_anim
+        temp_deck = []
+        (1..52).each {|i| temp_deck[i] = i}
+        @pile = temp_deck
+        @topcard = 0
+        @top_card.handle = @handles[0] # back of deck
+      end
+      button "quit" do
+        exit
+      end
+      button "Change Deck" do
+        fpath = ask_open_file
+        if fpath
+          init_with_path(fpath)
+          @top_card.handle = @handles[0]
+          @topcard = 0
+        end
+      end
+    end 
+  end 
 end
