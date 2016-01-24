68635c24556b6d873d80c3b2c2ab70afcc6303b5
passenger94 <pierredavant@gmail.com>
2016-01-24 23:39:41 +0100

    first draft @ fiddle video widget using vlc lib

--- shoes/native.h
+++ shoes/native.h
@@ -69,19 +69,20 @@ void shoes_native_control_focus(SHOES_CONTROL_REF);
 void shoes_native_control_state(SHOES_CONTROL_REF, SHOES_BOOL, SHOES_BOOL);
 void shoes_native_control_remove(SHOES_CONTROL_REF, shoes_canvas *);
 void shoes_native_control_free(SHOES_CONTROL_REF);
-SHOES_CONTROL_REF shoes_native_surface_new(shoes_canvas *, shoes_video *, shoes_place *);
+SHOES_CONTROL_REF shoes_native_surface_new2(VALUE);
+unsigned long shoes_native_surface_get_window_handle(SHOES_CONTROL_REF);
 void shoes_native_surface_position(SHOES_SURFACE_REF, shoes_place *,
   VALUE, shoes_canvas *, shoes_place *);
 void shoes_native_surface_hide(SHOES_SURFACE_REF);
 void shoes_native_surface_show(SHOES_SURFACE_REF);
 void shoes_native_surface_remove(shoes_canvas *, SHOES_SURFACE_REF);
 
-SHOES_SURFACE_REF shoes_native_svg(shoes_canvas *, VALUE, shoes_place *);
-void shoes_native_svg_position(SHOES_SURFACE_REF, shoes_place *,
-  VALUE, shoes_canvas *, shoes_place *);
-void shoes_native_svg_hide(SHOES_SURFACE_REF);
-void shoes_native_svg_show(SHOES_SURFACE_REF);
-void shoes_native_svg_remove(shoes_canvas *, SHOES_SURFACE_REF);
+//SHOES_SURFACE_REF shoes_native_svg(shoes_canvas *, VALUE, shoes_place *);
+//void shoes_native_svg_position(SHOES_SURFACE_REF, shoes_place *,
+//  VALUE, shoes_canvas *, shoes_place *);
+//void shoes_native_svg_hide(SHOES_SURFACE_REF);
+//void shoes_native_svg_show(SHOES_SURFACE_REF);
+//void shoes_native_svg_remove(shoes_canvas *, SHOES_SURFACE_REF);
 
 SHOES_CONTROL_REF shoes_native_button(VALUE, shoes_canvas *, shoes_place *, char *);
 SHOES_CONTROL_REF shoes_native_edit_line(VALUE, shoes_canvas *, shoes_place *, VALUE, char *);
