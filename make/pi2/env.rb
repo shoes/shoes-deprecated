6f8f8982d9dce0a7603173f426c08fee3742da76
Cecil <ccoupe@cableone.net>
2016-02-21 23:13:23 -0700

    pi2 build w/o gtk2 #206
    * still has memory of gtk2 in tasks.rb

--- make/pi2/env.rb
+++ make/pi2/env.rb
@@ -1,6 +1,4 @@
-# Build a 64 bit Linux Tight Shoes (from a 64 bit host)
-# In this case Unbuntu 14.04 to debian 7.2 in a chroot.
-# You should modify your custom.yaml
+# Build a 32 bit Linux Tight Shoes on a pi2
 cf =(ENV['ENV_CUSTOM'] || "#{TGT_ARCH}-custom.yaml")
 if File.exists? cf
   custmz = YAML.load_file(cf)
@@ -12,16 +10,13 @@ if File.exists? cf
   APP['EXTLIST'] = custmz['Exts'] if custmz['Exts']
   APP['GEMLIST'] = custmz['Gems'] if custmz['Gems']
   APP['INCLGEMS'] = custmz['InclGems'] if custmz['InclGems']
-  APP['GTK'] = custmz['Gtk'] if custmz['Gtk']
+  #APP['GTK'] = custmz['Gtk'] if custmz['Gtk']
 else
-  # define where your deps are
-  ShoesDeps = ""
-  EXT_RUBY = "/usr/local"
-  APP['GTK'] = "gtk+-2.0"
+  abort "missing custom.yaml"
 end
-APP['VIDEO'] = true
 #ENV['DEBUG'] = "true" # turns on the tracing log
 #ENV['GDB'] = "" # compile -g,  strip symbols when not defined
+APP['GTK'] = 'gtk+-3.0' # installer needs this to name the output
 CHROOT = ShoesDeps
 SHOES_TGT_ARCH = 'armv7l-linux-eabihf'
 SHOES_GEM_ARCH = "#{Gem::Platform.local}"
@@ -35,17 +30,12 @@ ularch = "#{TGT_SYS_DIR}usr/lib/#{arch}"
 larch = "#{TGT_SYS_DIR}lib/#{arch}"
 # Set appropriately
 CC = "gcc"
-#pkgruby ="#{EXT_RUBY}/lib/pkgconfig/ruby-2.1.pc"
 pkgruby ="#{EXT_RUBY}/lib/pkgconfig/ruby-2.2.pc"
-pkggtk ="#{ularch}/pkgconfig/#{APP['GTK']}.pc" 
+pkggtk ="#{ularch}/pkgconfig/gtk+-3.0.pc" 
 # Use Ruby or curl for downloads
 RUBY_HTTP = true
-if APP['GTK']== 'gtk+-2.0'
-  file_list = ["shoes/console/*.c"] + ["shoes/native/gtk.c"] + ["shoes/http/rbload.c"] + ["shoes/*.c"]
-else
-  file_list = ["shoes/console/*.c"] + ["shoes/native/*.c"] + ["shoes/http/rbload.c"] + ["shoes/*.c"]
-end 
-file_list << "shoes/video/video.c" if APP['VIDEO']
+file_list = ["shoes/console/*.c"] + ["shoes/native/*.c"] + ["shoes/http/rbload.c"] + ["shoes/*.c"]
+file_list << "shoes/video/video.c" 
 SRC = FileList[*file_list]
 OBJ = SRC.map do |x|
   x.gsub(/\.\w+$/, '.o')
@@ -66,10 +56,8 @@ if ENV['DEBUG'] || ENV['GDB']
 else
   LINUX_CFLAGS = " -O -Wall"
 end
-LINUX_CFLAGS << " -DRUBY_HTTP" if RUBY_HTTP
-LINUX_CFLAGS << " -DVIDEO" if APP['VIDEO']
+LINUX_CFLAGS << " -DRUBY_HTTP" 
 LINUX_CFLAGS << " -DSHOES_GTK -fPIC -Wno-unused-but-set-variable -Wno-unused-variable"
-LINUX_CFLAGS << " -DGTK3" unless APP['GTK'] == 'gtk+-2.0'
 LINUX_CFLAGS << " -I#{TGT_SYS_DIR}usr/include "
 LINUX_CFLAGS << `pkg-config --cflags "#{pkgruby}"`.strip+" "
 LINUX_CFLAGS << `pkg-config --cflags "#{pkggtk}"`.strip+" "
@@ -96,7 +84,6 @@ LINUX_LIBS = LINUX_LIB_NAMES.map { |x| "-l#{x}" }.join(' ')
 LINUX_LIBS << " #{CURL_LDFLAGS if !RUBY_HTTP} #{RUBY_LDFLAGS} #{CAIRO_LIB} #{PANGO_LIB} #{MISC_LIB}"
 
 SOLOCS = {}
-SOLOCS['curl'] = "#{curlloc}/libcurl.so.4" if !RUBY_HTTP
 SOLOCS['ungif'] = "#{uldir}/libungif.so.4" if !justgif
 SOLOCS['gif'] = "#{ularch}/libgif.so.4"  if justgif
 SOLOCS['jpeg'] = "#{ularch}/libjpeg.so.8"
@@ -105,5 +92,5 @@ SOLOCS['pcre'] = "#{larch}/libpcre.so.3"
 SOLOCS['crypto'] = "#{ularch}/libcrypto.so.1.0.0"
 SOLOCS['ssl'] = "#{ularch}/libssl.so.1.0.0"
 SOLOCS['sqlite'] = "#{ularch}/libsqlite3.so.0.8.6"
-SOLOCS['ffi'] = "#{ularch}/libffi.so"  if APP['VIDEO']
+SOLOCS['ffi'] = "#{ularch}/libffi.so" 
 SOLOCS['rsvg2'] = "#{ularch}/librsvg-2.so"
