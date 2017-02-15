d0a4317488639abe54a44e8bf1c50f2894744ff1
Ian Trudel <ian.trudel@gmail.com>
2017-02-15 12:53:03 -0500

    Migrating Search class to use Picky
    Search no longer relies on the defunct ftsearch.

--- lib/shoes/search.rb
+++ lib/shoes/search.rb
@@ -1,46 +1,53 @@
-require 'ftsearch/fragment_writer'
-require 'ftsearch/analysis/simple_identifier_analyzer'
-#require 'ftsearchrt'
+require("yajl")
+require("picky")
 
-class Shoes::Search
-  include FTSearch
-  attr_reader :index
-  def initialize fields = [:uri, :body]
-    field_infos = FTSearch::FieldInfos.new
-    fields.each do |name|
-      field_infos.add_field :name => name,
-        :analyzer => FTSearch::Analysis::SimpleIdentifierAnalyzer.new
-    end
-    @index = FTSearch::FragmentWriter.new :path => nil, :field_infos => field_infos
-  end
-  def add_document hsh
-    @index.add_document hsh
-  end
-  def finish!
-    @index.finish!
+Picky.logger = Picky::Loggers::Silent.new
 
-    @ft = FulltextReader.new :io => StringIO.new(@index.fulltext_writer.data)
-    @sa = SuffixArrayReader.new @ft, nil, :io => StringIO.new(@index.suffix_array_writer.data)
-    @dm = DocumentMapReader.new :io => StringIO.new(@index.doc_map_writer.data)
-  end
-  def find_all terms, show = 20, prob_sort = false
-    h = Hash.new{|h,k| h[k] = 0}
-    weights = Hash.new(1.0)
-    weights[0] = 10000000   # :uri
-    weights[1] = 10000000  # :body
-    hits = @sa.find_all terms
-    size = hits.size
-    if prob_sort && size > 10000
-      iterations = 50 * Math.sqrt(size)
-      offsets = @sa.lazyhits_to_offsets(hits)
-      weight_arr = weights.sort_by{|id,w| id}.map{|_,v| v}
-      sorted = @dm.rank_offsets_probabilistic(offsets, weight_arr, iterations)
-    else
-      offsets = @sa.lazyhits_to_offsets(hits)
-      sorted = @dm.rank_offsets(offsets, weights.sort_by{|id,w| id}.map{|_,v| v})
-    end
-    sorted[0..show].map do |doc_id, count|
-      [@dm.document_id_to_uri(doc_id), count]
-    end
-  end
+class Search
+   Document = Struct.new :id, :uri, :body
+   
+   Picky.root = LIB_DIR
+   
+   def initialize
+      @documents = []
+      @index = Picky::Index.new :terms do
+         indexing removes_characters: %r{[^a-z0-9\s\/\-\_\:\"\&\.]}i,
+            splits_text_on:     %r{[\s/\-\_\:\"\&/\.]}
+         category :uri, :from => lambda { |doc| doc.uri.dup }
+         category :body, :from => lambda { |doc| doc.body.dup }
+      end
+      @search = Picky::Search.new @index do 
+         searching removes_characters: %r{[^a-z0-9\s\/\-\_\:\"\&\.]}i,
+            splits_text_on:     %r{[\s/\-\_\:\"\&/\.]}
+      end
+      
+      @update = true
+      if File.directory?(File.join(Picky.root, "index"))
+         file = Dir[File.join(Picky.root, "index", "development", "terms", "*")].first
+         if (File.mtime("#{DIR}/static/manual-en.txt") < File.mtime(file))
+            @index.load
+            @update = false
+         end
+      end
+   end
+   
+   def add_document(terms = {})
+      @documents << Document.new(@documents.size + 1, terms[:uri], terms[:body])
+      @index.add @documents[-1] if @update
+   end
+   
+   def find_all(terms)
+      retval = []
+      results = @search.search(terms)
+      results.sort_by { |id| @documents.detect { |n| n.id == id }.uri =~ /#{terms}/ ? 0 : id }
+      results.ids.each do |id|
+         document = @documents.detect { |n| n.id == id }
+         retval << [document.uri] unless document.nil?
+      end
+      retval
+   end
+   
+   def finish!
+      @index.dump if @update
+   end
 end
