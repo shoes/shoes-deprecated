b7684d5ebc5b29220b8c7a86977f00c4184414ba
Cecil <ccoupe@cableone.net>
2017-04-28 22:24:32 -0600

    get windows theme working for xwin7 and win7 targets.
    * issue #345
    * some improvment for #334

--- make/win32/win7/env.rb
+++ make/win32/win7/env.rb
@@ -1,6 +1,6 @@
 require "devkit"
 cf =(ENV['ENV_CUSTOM'] || "win7-custom.yaml")
-gtk_version = '2'
+gtk_version = '3'
 if File.exists? cf
   custmz = YAML.load_file(cf)
   ShoesDeps = custmz['Deps']
@@ -38,18 +38,7 @@ console_list  = Dir["shoes/console/*.c"]
 file_list = %w{shoes/native/gtk.c shoes/http/rbload.c} + gtk_extra_list + ["shoes/*.c"] +
      ["shoes/plot/*.c"] + ["shoes/types/*.c"] + ["shoes/native/gtk/*.c"]
      
-#if APP['GTK'] == "gtk+-3.0"
-#  gtk_extra_list = %w(shoes/native/gtkfixedalt.c shoes/native/gtkentryalt.c
-#               shoes/native/gtkcomboboxtextalt.c shoes/native/gtkbuttonalt.c
-#               shoes/native/gtkscrolledwindowalt.c shoes/native/gtkprogressbaralt.c )
-#end
-#if RUBY_HTTP
-#  file_list = %w{shoes/native/gtk.c shoes/http/rbload.c} + gtk_extra_list + ["shoes/*.c"] +
-#     ["shoes/plot/*.c"]
-#else
-#  file_list = %w{shoes/native/gtk.c shoes/http/winhttp.c shoes/http/windownload.c} + ["shoes/*.c"] 
-#end
-#file_list << "shoes/video/video.c" if APP['VIDEO']
+
 SRC = FileList[*file_list]
 
 OBJ = SRC.map do |x|
@@ -95,10 +84,7 @@ WIN32_CFLAGS << "-I#{ShoesDeps}/include/librsvg-2.0/librsvg "
 RbConfig::CONFIG.select { |k, _| k[/hdrdir/] }.each_key do |v|
    WIN32_CFLAGS << "-I#{RbConfig::CONFIG[v]}"
 end
-#["jpeg-6b-4-lib", "giflib-4.1.4-1-lib"].each { |n|
-#   WIN32_CFLAGS << "-Isandbox/#{n}/include"
-#   WIN32_LDFLAGS << "-Lsandbox/#{n}/lib"
-#}
+
 WIN32_CFLAGS << "-Ishoes"
 
 WIN32_LDFLAGS << "-lshell32 -lkernel32 -luser32 -lgdi32 -lcomdlg32 -lcomctl32"
@@ -110,9 +96,6 @@ WIN32_LDFLAGS << CAIRO_LDFLAGS
 WIN32_LDFLAGS << PANGO_LDFLAGS
 WIN32_LDFLAGS << RUBY_LDFLAGS
 
-#WIN32_LIBS = WIN32_LDFLAGS
-#WIN32_LIBS << "-L#{ENV['RI_DEVKIT']}/mingw/bin".gsub('\\','/').gsub(/^\//,'//')
-#WIN32_LIBS << "-lgif -ljpeg -Wl,-export-all-symbols -lmsvcrt-ruby210 -lcairo -lpango-1.0 -lgobject-2.0 -lgmodule-2.0 -lgthread-2.0 -lglib-2.0 -lintl "
 WIN32_LIBS << RUBY_LDFLAGS
 WIN32_LIBS << CAIRO_LDFLAGS
 WIN32_LIBS << PANGO_LDFLAGS
@@ -180,35 +163,4 @@ if APP['GTK'] == 'gtk+-3.0'
       'sjlj'        => "#{devdll}/libgcc_s_sjlj-1.dll" 
     }
   )
-end
-if APP['GTK'] == 'gtk+-2.0'
-  SOLOCS.merge!(
-  {
-    'atk'         => "#{bindll}/libatk-1.0-0.dll",
-    'cairo'       => "#{bindll}/libcairo-2.dll",
-    'cairo-gobj'  => "#{bindll}/libcairo-gobject-2.dll",
-    'fontconfig'  => "#{bindll}/libfontconfig-1.dll",
-    'freetype'    => "#{bindll}/libfreetype-6.dll",
-    'gdkpixbuf'   => "#{bindll}/libgdk_pixbuf-2.0-0.dll",
-    'gdk2'        => "#{bindll}/libgdk-win32-2.0-0.dll",
-    'gio'         => "#{bindll}/libgio-2.0-0.dll",
-    'glib'        => "#{bindll}/libglib-2.0-0.dll",
-    'gmodule'     => "#{bindll}/libgmodule-2.0-0.dll",
-    'gobject'     => "#{bindll}/libgobject-2.0-0.dll",
-    'gtk2'        => "#{bindll}/libgtk-win32-2.0-0.dll",
-     'intl8'       => "#{bindll}/libintl-8.dll",
-    'pango'       => "#{bindll}/libpango-1.0-0.dll",
-    'pangocairo'  => "#{bindll}/libpangocairo-1.0-0.dll",
-    'pangoft'     => "#{bindll}/libpangoft2-1.0-0.dll",
-    'pango32'     => "#{bindll}/libpangowin32-1.0-0.dll",
-    'harfbuzz'    => "#{bindll}/libharfbuzz-0.dll",
-    'pixman'      => "#{bindll}/libpixman-1-0.dll",
-    'png16'       => "#{bindll}/libpng16-16.dll",
-    'xml2'        => "#{bindll}/libxml2-2.dll",
-    'thread'      => "#{bindll}/libgthread-2.0-0.dll",
-    'zlib1'       => "#{bindll}/zlib1.dll",
-    'pthread'     => "#{devdll}/libwinpthread-1.dll",
-    'sjlj'        => "#{devdll}/libgcc_s_sjlj-1.dll"
-  }
-)
 end
