0de400aaabecec8bd55a687bca504d503fc56eb0
Cecil <ccoupe@cableone.net>
2017-06-01 20:58:24 -0600

    osx xmavericks build was broken  by a commit a few weeks ago.
      * now works again.

--- make/darwin/loose/tasks.rb
+++ make/darwin/loose/tasks.rb
@@ -1,4 +1,28 @@
-require File.expand_path('make/make')
+module Make
+  include FileUtils
+
+  def cc(t)
+    sh "#{CC} -I. -c -o#{t.name} #{LINUX_CFLAGS} #{t.source}"
+  end
+
+  # Subs in special variables
+  def rewrite before, after, reg = /\#\{(\w+)\}/, reg2 = '\1'
+    File.open(after, 'w') do |a|
+      File.open(before) do |b|
+        b.each do |line|
+          a << line.gsub(reg) do
+            if reg2.include? '\1'
+              reg2.gsub(%r!\\1!, Object.const_get($1))
+            else
+              reg2
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
 
 include FileUtils
 
