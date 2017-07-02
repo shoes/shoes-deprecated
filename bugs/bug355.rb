31d80d9552fe509d8317f5b127b6ff3a721314a9
Cecil <ccoupe@cableone.net>
2017-07-01 20:49:02 -0600

    for #355 - slightly different test script

---
+++ bugs/bug355.rb
@@ -0,0 +1,9 @@
+Shoes.app(title: " v1.0", width: 300, height: 500, resizable: true ) do
+    background green
+    @main = stack left: 0.05, top: 0.15, width: 0.9, height: 0.8, scroll: true do
+        background beige
+        100.times do |i|
+            para "#{i+1} times"
+        end
+    end
+end
