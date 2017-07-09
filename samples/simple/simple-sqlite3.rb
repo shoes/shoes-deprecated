82c348f6f3073c4236a891230b1f416847353501
Cecil <ccoupe@cableone.net>
2017-07-08 23:19:10 -0600

    fix samples/simple/simple-sqlite3.rb to work better

--- samples/simple/simple-sqlite3.rb
+++ samples/simple/simple-sqlite3.rb
@@ -1,6 +1,9 @@
+# encoding: utf-8
 require 'sqlite3'
 require 'fileutils'
+include FileUtils
 tmpdir = File.join(LIB_DIR,"sample-data")
+#$stderr.puts "tmpdir #{tmpdir}"
 mkdir_p tmpdir
 dbfile = File.join(tmpdir, "simple-sqlite3.db")
 if File.exists? dbfile
@@ -14,8 +17,8 @@ Shoes.app :width => 350, :height => 130 do
     "TEXT,num double,timeEnter DATE)"
   db.execute "insert into t1 (data,num) values ('This is sample data',3)"
   db.execute "insert into t1 (data,num) values ('More sample data',6)"
-  #db.execute "insert into t1 (data,num) values ('Aurélio, Küng, Stärk, Uña, Łuksza',6)"
-  db.execute "insert into t1 (data,num) values ('Aurlio, Kng, Strk, Ua, uksza',6)"
+  db.execute "insert into t1 (data,num) values ('Aurélio, Küng, Stärk, Uña, Łuksza',6)"
+  #db.execute "insert into t1 (data,num) values ('Aurlio, Kng, Strk, Ua, uksza',6)"
   db.execute "insert into t1 (data,num) values ('And a little more',9)"
   rows = db.execute "select * from t1"
   rows.each{|k, d, n| para "#{k} : #{d} : #{n}\n"}
